c7bc3c9622f5507091d93562e82befe1bc4940f9
Soumik Bag <bagsoumik6@gmail.com>
2025-06-26 12:41:39 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler =(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(req,res,next).catch((error)=>next(error))
+    }
+}
+
+export {asyncHandler}
+
+
+// Try-Catch
+// const asyncHandler =(func)=>async(req,res,next)=>{
+//     try {
+//         await func(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 404).json(
+//             {
+//                 success : false,
+//                 message : error.message
+//             }
+//         )
+//     }
+// }
